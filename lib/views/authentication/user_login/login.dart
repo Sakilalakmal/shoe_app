e1dcc2aa9f979c13a1312f0b470ff385ff470765
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-22 14:41:34 +0530

    update about us screen

--- lib/views/authentication/user_login/login.dart
+++ lib/views/authentication/user_login/login.dart
@@ -8,6 +8,7 @@ import 'package:shoe_app_assigment/utils/theme/sizes.dart';
 import 'package:shoe_app_assigment/views/authentication/user_login/widgets/login_form.dart';
 import 'package:shoe_app_assigment/views/authentication/user_login/widgets/login_header.dart';
 import 'package:shoe_app_assigment/views/authentication/vendor_login/vendor_login.dart';
+import 'package:shoe_app_assigment/views/screen/inner_screens/widget/about_us_wrapper.dart';
 
 
 
@@ -72,7 +73,9 @@ class LoginScreen extends StatelessWidget {
                     }));
                   }, child: Text(Texts.signIn),)
                 ],
-              )
+              ),
+
+              AboutUsWrapper(),
             ],
           ),
         ),
