9bc1138d4fb2e7c38b68fedbe302a2af92a11eca
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-22 18:23:36 +0530

    delete about_us.json file

--- lib/controllers/connectivity_controller/connectivity_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-
-class ConnectivityController {
-  // Function to check connectivity status
-  Future<bool> checkConnectivity() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-    return connectivityResult != ConnectivityResult.none; // true if online, false if offline
-  }
-}
